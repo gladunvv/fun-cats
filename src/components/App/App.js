fe91665a018eb2f9724573abc87233631387aaae
vladislav <bincha.1997@gmail.com>
2020-09-05 23:36:10 +1000

    feat: update project

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,18 @@
-import React from "react";
-import "./App.css";
+import React from 'react';
+import { Title } from '../Title/Title';
+import { Card } from '../Card/Card';
+
+import './App.css';
 
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className='container'>
+      <Title />
+      <div className='card-wrapper'>
+        <Card />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './Title.css';
+
+export const Title = () => {
+  return <h2 className='title'>Ты сегодня покормил кота?</h2>;
+};
