bc08afc46a0c35652605da692132997e037d6262
vladislav <bincha.1997@gmail.com>
2020-09-08 23:55:52 +1000

    feat: update

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,5 +3,15 @@ import React from 'react';
 import './Card.css';
 
 export const Card = () => {
-  return <div className='card'></div>;
+  return (
+    <div className='card'>
+      <span className='card__text'>Сказачное заморское яство</span>
+      <h3 className='card__title'>Нямушка</h3>
+      <span className='card__sub-title'>с фуа-гра</span>
+      <br />
+      <span className='card__sub-text'>10 порций мышь в подарок </span>
+
+      <div className='card__circle'>0,5 кг</div>
+    </div>
+  );
 };
